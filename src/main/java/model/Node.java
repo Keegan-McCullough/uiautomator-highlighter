18aa061b65064f9c6dc5dead5e3fbd41c9198c76
Keegan-McCullough <keeganmcculloughfury@gmail.com>
2025-09-05 17:33:22 -0400

    Created node constructor and filled out its variables

--- src/main/java/model/Node.java
+++ src/main/java/model/Node.java
@@ -1,8 +1,23 @@
 package model;
 
-public class Node{
-/*
-need to create the nodes that hold text its children and
-the rectangle to be highlighted
- */
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    /*
+    need to create the nodes that hold text its children and
+    the rectangle to be highlighted
+     */
+    private String className;
+    private String text;
+    private Rectangle bounds;
+    private List<Node> children;
+
+    public Node(String className, String text, String boundary) {
+        this.className = className;
+        this.text = text;
+        //this.bounds = parseBounds(boundsStr);
+        this.children = new ArrayList<>();
+    }
 }
