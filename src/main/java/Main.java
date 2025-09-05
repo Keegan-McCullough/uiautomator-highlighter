080341a45b2af83ee0f9997765881daa34b8b169
Keegan-McCullough <keeganmcculloughfury@gmail.com>
2025-09-05 13:23:29 -0400

    Initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import image.ImageAnnotator;
+import model.Node;
+import parser.XmlParser;
+
+import java.io.File;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        System.out.println("hello world");
+    }
+}
