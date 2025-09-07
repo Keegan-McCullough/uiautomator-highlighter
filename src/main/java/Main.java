02e661802b19f8ca787a33b242790c07829e7484
Keegan-McCullough <keeganmcculloughfury@gmail.com>
2025-09-06 21:17:52 -0400

    Added for loop for parsing that looks in the input directory for files and parses them one at a time.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,17 +9,31 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        String inputXml = "input/com.giphy.messenger-1.xml";
-        String inputPng = "input/com.giphy.messenger-1.png";
-        String outputPng = "output/com.giphy.messenger-1.png";
-        System.out.println("hello world");
-
-        // Parse XML into tree of UiNodes
+        // Initialize helpers
         XmlParser parser = new XmlParser();
-        Node root = parser.parse(inputXml);
 
-        List<Node> leafNodes = root.getLeafNodes();
-        System.out.println("Number of nodes:" + leafNodes.size());
-        ImageAnnotator.annotate(inputPng, outputPng,leafNodes);
+        // Get all files in input
+        File dir = new File("input");
+        File[] files = dir.listFiles((d, name) -> name.endsWith(".xml"));
+
+        if (files == null) {
+            System.out.println("No input files found.");
+            return;
+        }
+
+        // For every XML file, find matching PNG and process
+        for (File xmlFile : files) {
+            String baseName = xmlFile.getName().replace(".xml", "");
+            String inputXml = "input/" + baseName + ".xml";
+            String inputPng = "input/" + baseName + ".png";
+            String outputPng = "output/" + baseName + ".png";
+
+            // Parse XML into node tree
+            Node root = parser.parse(inputXml);
+            List<Node> leafNodes = root.getLeafNodes();
+
+            System.out.println(inputXml);
+            ImageAnnotator.annotate(inputPng, outputPng, leafNodes);
+        }
     }
 }
